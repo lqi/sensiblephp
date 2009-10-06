ff641e63cb1f8f40d74f699c80bb985de1763d56
lqi <lqi@localhost>
2009-10-06 00:04:02 +0000

    Try to share the remote MySQL database.

--- conf/DbConf.class.php
+++ conf/DbConf.class.php
@@ -1,10 +1,10 @@
 <?php
 class DbConf {
-	private $host = "localhost";
+	private $host = "fazey.org";
 	private $port = "";
-	private $user = "root";
-	private $password = "root";
-	private $dbname = "sensible";
+	private $user = "cents";
+	private $password = "pl0x88!";
+	private $dbname = "cents";
 	
 	public function __get($key) {
 		return $this->$key;
