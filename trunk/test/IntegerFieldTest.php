4b8ed7e220828cdf0e45c694848826b2871496ce
lqi <lqi@localhost>
2009-10-18 00:04:58 +0000

    IntegerField added and test cases

---
+++ trunk/test/IntegerFieldTest.php
@@ -0,0 +1,40 @@
+<?php
+require_once 'init.php';
+ 
+class IntegerFieldTest extends PHPUnit_Framework_TestCase
+{
+	private $integerField;
+	
+	protected function setUp() {
+		$this->integerField = new IntegerField;
+	}
+	
+	function testGetFieldType() {
+		$this->assertEquals("IntegerField", $this->integerField->getFieldType());
+	}
+	
+	function testGetValue() {
+		$this->integerField->setValue(1);
+		$this->assertEquals(2, $this->integerField->getValue() + 1);
+	}
+	
+	function testSetChar() {
+		try {
+			$this->integerField->setValue("a");
+		}
+		catch (Exception $ex) {
+			return;
+		}
+		$this->fail("Exception expected: Set Characters to Integer Field.");
+	}
+	
+	function testSetFloat() {
+		try {
+			$this->integerField->setValue(1.11);
+		}
+		catch (Exception $ex) {
+			return;
+		}
+		$this->fail("Exception expected: Set Float to Integer Field.");
+	}
+}
