d3f5ec0d9d23bb3979e8fd8967a8d2f4fdc9e4eb
lqi <lqi@localhost>
2009-11-21 21:14:50 +0000

    Change DbConf to Settings, make Settings more cohesive

---
+++ conf/Settings.class.php
@@ -0,0 +1,25 @@
+<?php
+class Settings {
+	/*
+		General Settings
+	*/
+	private $debug = false;			//Debug model, choose true or false
+	private $timezone = "";			//Leave blank for "America/Los_Angeles"
+	
+	/*
+		Database Settings
+	*/
+	private $host = "fazey.org";	//Host of Database, leave blank for "localhost"
+	private $port = "";				//Port of Database, leave blank for null
+	private $user = "cents";		//Username of Database, leave blank for null
+	private $password = "pl0x88!";	//Password of Database, leave blank for null
+	private $dbname = "cents";		//Database name, leave blank for null
+	
+	/*
+		Don't modify anything below
+	*/
+	public function __get($key) {
+		return $this->$key;
+	}
+}
+?>
--- web/index.php
+++ web/index.php
@@ -1,6 +1,3 @@
 <?php
-	define("DEBUG", true);
-	define("TIMEZONE", "America/Los_Angeles");
-	
 	require(dirname(getcwd()) . "/core/init.php");
 ?>
