70434502b8e50e6feb70b4683b748dc019991a96
lqi <lqi@localhost>
2009-11-21 18:23:07 +0000

    make Data Time Datatime DRYer

--- trunk/controllers/AdminController.class.php
+++ trunk/controllers/AdminController.class.php
@@ -17,16 +17,9 @@ class AdminController extends Controller {
 	function bloginsertAction() {
 		$title = $this->fetchPost("title");
 		$body = $this->fetchPost("body");
-		//the following will be refactored
-		$year = date("Y");
-		$month = date("m");
-		$day = date("d");
-		$hour = date("H");
-		$minute = date("i");
-		$second = date("s");
-		// the above will be refactored
+		
 		$blog = new Blog;
-		$blog->date->setValue($year, $month, $day, $hour, $minute, $second);
+		$blog->date->setValue();
 		$blog->title->setValue($title);
 		$blog->body->setValue($body);
 		
@@ -56,16 +49,7 @@ class AdminController extends Controller {
 		$date = $blogDb->get($id)->date->getValue();
 		
 		$blog = new Blog;
-		//the following will be refactored
-		$blog->id->setValue((int) $id);
-		$year = substr($date, 0, 4);
-		$month = substr($date, 5, 2);
-		$day = substr($date, 8, 2);
-		$hour = substr($date, 11, 2);
-		$minute = substr($date, 14, 2);
-		$second = substr($date, 17, 2);
-		$blog->date->setValue($year, $month, $day, $hour, $minute, $second);
-		// the above will be refactored
+		$blog->date->processingPDOValue($date);
 		$blog->title->setValue($title);
 		$blog->body->setValue($body);
 		
