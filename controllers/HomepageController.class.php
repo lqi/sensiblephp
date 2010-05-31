5e4eeea766e03bb44d17b474e3f63f14355b5c51
lqi <lqi@localhost>
2010-05-31 10:23:36 +0000

    change HomepageController to set parameters based on account privilege

--- controllers/HomepageController.class.php
+++ controllers/HomepageController.class.php
@@ -7,12 +7,15 @@ class HomepageController extends Controller {
 		if ($account) {
 			$this->setValue("account", $account);
 			$user_id = $account->user_id->getValue();
-			$infoDb = new BasicInfoDb;
-			$info = $infoDb->basicInfoFromUserId($user_id);
-			$this->setValue("info", $info);
-			$appDb = new ApplicationDb;
-			$application = $appDb->applicationStatusFromUserId($user_id);
-			$this->setValue("application", $application);
+			$privilege = $account->privilege->getValue();
+			if ($privilege == 4) {
+				$infoDb = new BasicInfoDb;
+				$info = $infoDb->basicInfoFromUserId($user_id);
+				$this->setValue("info", $info);
+				$appDb = new ApplicationDb;
+				$application = $appDb->applicationStatusFromUserId($user_id);
+				$this->setValue("application", $application);
+			}
 		}
 	}
 }
